05981b230873bf6cc4f4bf0c960e6b554759713a
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2023-07-07 21:59:01 -0500

    First commit

---
+++ get_calculos.R
@@ -0,0 +1,119 @@
+library(lubridate)
+library(readxl)
+library(dplyr)
+
+# This is the data where all the information comes from
+datos <- read_excel("data.xlsx")
+
+#' Get the values we need to include in the daily notes of the newborns at the
+#' intensive care unit of neonatology
+#' 
+#' This function includes some calculations to obtain some variables that are
+#' important.
+#'
+#' @param paciente Name of the patient, used to find the data in the data set.
+#' @param fecha Date of the note we want to make (it uses the values of the
+#'   previous day for most of the variables).
+#'
+#' @return A list containing values of different variables that are useful to
+#'   make the daily notes of the patients
+#' @export
+#'
+#' @examples
+#' get_calculos("larico", fecha = as_date("2023-07-03"))
+#' 
+get_calculos <- function(paciente, fecha = today()) {
+  
+  fila_hoy <- datos[datos$paciente == paciente & datos$fecha == fecha, ]
+  fila_ayer <- datos[datos$paciente == paciente & datos$fecha == fecha - 1, ]
+  
+  simplify <- function (x) {
+    unname(unlist(x))
+  }
+  
+  ingresos_y_egresos <- fila_ayer %>%
+    select(starts_with("ingreso"), starts_with("egreso"))
+  peso_nacer <- select(fila_hoy, peso_nacer) %>% simplify()
+  peso_hoy <- select(fila_hoy, peso) %>% simplify()
+  peso_ayer <- select(fila_ayer, peso) %>% simplify()
+  delta_peso <- peso_hoy - peso_ayer
+  delta_peso_nacer <- (peso_hoy - peso_nacer) / peso_nacer
+  
+  total_ingresos <- fila_ayer %>%
+    select(starts_with("ingreso")) %>%
+    summarize(sum_all = rowSums(.)) %>%
+    simplify()
+  
+  get_perdidas_insensibles <- function(peso_hoy, peso_nacer) {
+    # Peso por usar
+    if (peso_hoy < peso_nacer) {
+      peso <- peso_nacer
+    } else {
+      peso <- peso_hoy
+    }
+    # Constante
+    if (peso < 750) {
+      k <- 2.5
+    } else if (peso >= 750 & peso < 1000) {
+      k <- 2
+    } else if (peso >= 1000 & peso < 1500) {
+      k <- 1.5
+    } else if (peso >= 1500 & peso < 2000) {
+      k <- 1
+    } else if (peso >= 2000 & peso < 2500) {
+      k <- 0.75
+    } else if (peso >= 2500) {
+      k <- 0.5
+    }
+    return(peso * 24 * k)
+  }
+  
+  perdidas_insensibles <- get_perdidas_insensibles(peso_hoy, peso_nacer) / 1000
+  total_egresos <- fila_ayer %>%
+    select(egreso_deposiciones, egreso_diuresis_24, egreso_residuos) %>%
+    summarize(sum_all = rowSums(.)) %>%
+    simplify()
+  
+  balance <- total_ingresos - total_egresos
+  
+  if (peso_hoy < peso_nacer) {
+    volumen_total_efectivo <- total_ingresos / peso_nacer * 1000
+  } else {
+    volumen_total_efectivo <- total_ingresos / peso_hoy * 1000
+  }
+  
+  via_oral_efectiva <- fila_ayer %>%
+    summarize(result = ingreso_via_oral / peso_hoy * 1000) %>%
+    simplify()
+  
+  flujo_urinario <- fila_ayer %>%
+    mutate(
+      flujo_24h = egreso_diuresis_24 / peso_hoy * 1000 / 24,
+      flujo_12h = egreso_diuresis_12 / peso_hoy * 1000 / 12,
+      flujo_06h = egreso_diuresis_06 / peso_hoy * 1000 / 6,
+      .keep = "none"
+    )
+  
+  list(
+    ingreso_endovenoso = ingresos_y_egresos$ingreso_endovenoso,
+    ingreso_tratamiento = ingresos_y_egresos$ingreso_tratamiento,
+    ingreso_via_oral = ingresos_y_egresos$ingreso_via_oral,
+    egreso_deposiciones = ingresos_y_egresos$egreso_deposiciones,
+    egreso_diuresis_24 = ingresos_y_egresos$egreso_diuresis_24,
+    egreso_residuos = ingresos_y_egresos$egreso_residuos,
+    peso_nacer = peso_nacer,
+    peso_ayer = peso_ayer,
+    peso_hoy = peso_hoy,
+    delta_peso = delta_peso,
+    delta_peso_nacer = delta_peso_nacer,
+    total_ingresos = total_ingresos,
+    perdidas_insensibles = perdidas_insensibles,
+    total_egresos = total_egresos,
+    balance = balance,
+    volumen_total_efectivo = volumen_total_efectivo,
+    via_oral_efectiva = via_oral_efectiva,
+    diuresis_flujo_24h = flujo_urinario$flujo_24h,
+    diuresis_flujo_12h = flujo_urinario$flujo_12h,
+    diuresis_flujo_06h = flujo_urinario$flujo_06h
+  )
+}
---
+++ get_calculos_text.R
@@ -0,0 +1,34 @@
+library(glue)
+
+#' Get the text to copy and paste in the daily notes of the patients
+#' 
+#' Note: this function depends on the main function `get_calculos()`.
+#' 
+#' This function creates a text that is ready to copy and paste in the daily
+#' notes of the patients.
+#' 
+#' The arguments are the same as the function `get_calculos()`.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' get_calculos_text("larico", fecha = as_date("2023-07-03"))
+#' 
+get_calculos_text <- function(paciente, fecha = today()) {
+  result <- get_calculos(paciente, fecha)
+  glue(
+    "PESO AL NACER: {result$peso_nacer} g // PESO AYER: {result$peso_ayer} g // PESO HOY: {result$peso_hoy} g
+DELTA PESO: {result$delta_peso} g // DELTA PESO AL NACER: {result$delta_peso_nacer * 100}%
+
+BALANCE 24 HORAS: {result$balance} cc
+INGRESOS (24 HORAS): {result$total_ingresos} cc
+= EV {result$ingreso_endovenoso} + TTO {result$ingreso_tratamiento} + VO {result$ingreso_via_oral}
+EGRESOS (24 HORAS): {result$total_egresos} cc
+= DEPOSICIONES {result$egreso_deposiciones} + DIURESIS {result$egreso_diuresis_24} + RESIDUOS {result$egreso_residuos} + PÉRDIDAS INSENSIBLES {result$perdidas_insensibles}
+
+VOLUMEN TOTAL EFECTIVO: {result$volumen_total_efectivo} cc/kg
+VÍA ORAL EFECTIVA: {round(result$via_oral_efectiva, 2)} cc/kg
+FLUJO URINARIO (cc/kg) EN 24 HORAS {round(result$diuresis_flujo_24h, 2)} // EN 12 HORAS {round(result$diuresis_flujo_12h, 2)} // EN 6 HORAS {round(result$diuresis_flujo_06h, 2)}"
+  )
+}
