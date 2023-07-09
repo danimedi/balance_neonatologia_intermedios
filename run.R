bc10db486f404def124fb006d0bb72a7210184a9
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2023-07-09 18:31:02 -0500

    Create a file to run the functions interactively

---
+++ run.R
@@ -0,0 +1,17 @@
+# INSTRUCCIONES DE USO:
+
+# Colocar aquí (entre las comillas) el nombre del paciente, tal cual lo hayamos
+# colocado en la base de datos:
+paciente <- "sandoval"
+
+# Luego darle apretar el botón de "Source" o usar las teclas "Alt + Ctrl + R".
+
+
+
+# ---------------------
+
+# No tocar nada de esto de acá abajo :D
+source("get_calculos.R")
+source("get_calculos_text.R")
+
+get_calculos_text(paciente)
