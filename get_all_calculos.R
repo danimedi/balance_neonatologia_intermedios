77c059f4e55fa8f585536c76d0f2f050a7e11f91
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2023-07-11 20:10:44 -0500

    Add function to perform the calculations for every patient with available information for a certain date

---
+++ get_all_calculos.R
@@ -0,0 +1,27 @@
+library(lubridate)
+library(purrr)
+
+#' Get the results of the balance calculations for all the patients as text
+#' 
+#' This functions makes the calculations for all the patient in a certain date,
+#' most commonly used for today's date.
+#' 
+#' It uses (depends on) the functions  `get_calculos()` and `get_calculos_text()`.
+#'
+#' @param db Data base containing the data to perform the calculations.
+#' @param fecha  Date to select the patients with available information.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' datos <- read_excel("data.xlsx")
+#' get_all_calculos(datos, fecha = as_date("2023-07-03"))
+get_all_calculos <- function(db, fecha = today()) {
+  pacientes_hoy <- db[db$fecha == fecha, "paciente"]
+  pacientes_hoy <- unlist(unique(pacientes_hoy), use.names = FALSE)
+  res <- map_chr(pacientes_hoy, function(paciente) get_calculos_text(db, paciente, fecha))
+  res <- paste0("PACIENTE: ", pacientes_hoy, "\n", res)
+  res <- paste0(res, collapse = "\n\n")
+  res
+}
--- run.R
+++ run.R
@@ -1,17 +1,7 @@
-# INSTRUCCIONES DE USO:
-
-# Colocar aquí (entre las comillas) el nombre del paciente, tal cual lo hayamos
-# colocado en la base de datos:
-paciente <- "sandoval"
-
-# Luego darle apretar el botón de "Source" o usar las teclas "Alt + Ctrl + R".
-
-
-
-# ---------------------
-
-# No tocar nada de esto de acá abajo :D
 source("get_calculos.R")
 source("get_calculos_text.R")
+source("get_all_calculos.R")
 
-get_calculos_text(paciente)
+library(readxl)
+datos <- read_excel("data.xlsx")
+get_all_calculos(datos)
