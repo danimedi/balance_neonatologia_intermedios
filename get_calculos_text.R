346f15757bb1dc75486a5c2dbc5786af1d8a87d4
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2023-07-19 17:33:19 -0500

    Read the template from file instead of function

--- get_calculos_text.R
+++ get_calculos_text.R
@@ -18,19 +18,6 @@ library(glue)
 #' 
 get_calculos_text <- function(...) {
   result <- get_calculos(...)
-  
-  glue(
-    "PESO AL NACER: {result$peso_nacer} g // PESO AYER: {result$peso_ayer} g // PESO HOY: {result$peso_hoy} g
-DELTA PESO: {result$delta_peso} g // DELTA PESO AL NACER: {result$delta_peso_nacer * 100}%
-
-BALANCE 24 HORAS: {result$balance} cc
-INGRESOS (24 HORAS): {result$total_ingresos} cc
-= EV {result$ingreso_endovenoso} + TTO {result$ingreso_tratamiento} + VO {result$ingreso_via_oral}
-EGRESOS (24 HORAS): {result$total_egresos} cc
-= DEPOSICIONES {result$egreso_deposiciones} + DIURESIS {result$egreso_diuresis_24} + RESIDUOS {result$egreso_residuos} + PÉRDIDAS INSENSIBLES {result$perdidas_insensibles}
-
-VOLUMEN TOTAL EFECTIVO: {result$volumen_total_efectivo} cc/kg
-VÍA ORAL EFECTIVA: {result$via_oral_efectiva} cc/kg
-FLUJO URINARIO (cc/kg) EN 24 HORAS {result$diuresis_flujo_24h} // EN 12 HORAS {result$diuresis_flujo_12h} // EN 6 HORAS {result$diuresis_flujo_06h}"
-  )
+  text <- readLines("plantilla_texto_acosta.txt")
+  glue(text)
 }
