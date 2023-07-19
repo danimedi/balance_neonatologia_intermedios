d6839d806a990747bc3697776e213b690cd284c4
DaniMedi <danielmedinaneira555@gmail.com>
2023-07-19 07:09:52 -0500

    Fix template of text

--- get_calculos_text.R
+++ get_calculos_text.R
@@ -20,17 +20,11 @@ get_calculos_text <- function(...) {
   result <- get_calculos(...)
   
   glue(
-    "PESO AL NACER: {result$peso_nacer} g // PESO AYER: {result$peso_ayer} g // PESO HOY: {result$peso_hoy} g
-DELTA PESO: {result$delta_peso} g // DELTA PESO AL NACER: {result$delta_peso_nacer * 100}%
+    "PN: {result$peso_nacer} G / P AYER: {result$peso_ayer} G / P HOY: {result$peso_hoy} G / DP: {result$delta_peso} G / DPN: {result$delta_peso_nacer * 100}%
 
-BALANCE 24 HORAS: {result$balance} cc
-INGRESOS (24 HORAS): {result$total_ingresos} cc
-= EV {result$ingreso_endovenoso} + TTO {result$ingreso_tratamiento} + VO {result$ingreso_via_oral}
-EGRESOS (24 HORAS): {result$total_egresos} cc
-= DEPOSICIONES {result$egreso_deposiciones} + DIURESIS {result$egreso_diuresis_24} + RESIDUOS {result$egreso_residuos} + PÉRDIDAS INSENSIBLES {result$perdidas_insensibles}
-
-VOLUMEN TOTAL EFECTIVO: {result$volumen_total_efectivo} cc/kg
-VÍA ORAL EFECTIVA: {result$via_oral_efectiva} cc/kg
-FLUJO URINARIO (cc/kg) EN 24 HORAS {result$diuresis_flujo_24h} // EN 12 HORAS {result$diuresis_flujo_12h} // EN 6 HORAS {result$diuresis_flujo_06h}"
+BH24H: {result$balance} ML (VTE: {result$volumen_total_efectivo} ML/KG/DIA)
+I: VO {result$ingreso_via_oral} + EV {result$ingreso_endovenoso} + TTO {result$ingreso_tratamiento} = {result$total_ingresos} ML
+E: O {result$egreso_diuresis_24} + DEPO {result$egreso_deposiciones} + RG {result$egreso_residuos} + PI {result$perdidas_insensibles} = {result$total_egresos} ML
+FU 24/12/6H: {result$diuresis_flujo_24h}/{result$diuresis_flujo_12h}/{result$diuresis_flujo_06h} ML/KG/H"
   )
 }
