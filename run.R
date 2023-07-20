000ad9ee888cdfecc59cd10d40c92fcfb8aecb92
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2023-07-19 19:03:45 -0500

    Simplify file

--- run.R
+++ run.R
@@ -5,8 +5,5 @@ source("get_all_calculos.R")
 library(readxl)
 datos <- read_excel("data.xlsx")
 
-get_calculos(datos, "alvarez canaza")
-get_calculos_text(datos, "alvarez canaza")
-
 txt <- get_all_calculos(datos)
 writeLines(txt, con = "output.txt")
